c7689d575dec9372bea8b6371ed9d1e5ba1dc27f
Ricardo Barbosa <ric3639@gmail.com>
2020-08-05 16:37:21 -0700

    post request

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const express = require('express');
+let db = require("./db.js")
 const app = express()
 
+app.use(express.json())
+
+
 //routes
+
+//post request to /api/users
+app.post("/api/users", (req,res) => {
+   const newUser = req.body;
+
+   //when request body is missing name or bio
+   db.insert(newUser)
+      .then(user=>{
+         res.status(200).json({success:"user insert success", user})
+      })
+      .catch(error=>{
+         res.status(500).json({errorMessage: "failed to insert user", error})
+      })
+}); //post request 
+
+//get request to /api/users
 
 
 
