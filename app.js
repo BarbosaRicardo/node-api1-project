1efe4457d1d8e10793ae5e6f1464250f209e8136
Ricardo Barbosa <ric3639@gmail.com>
2020-08-05 16:33:25 -0700

    db.js created

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express()
+
+//routes
+
+
+
+
+
+
+
+
+
+
+
+
+const port = 4000
+
+app.listen(port, () =>{
+   console.log("Express is running...")
+})
